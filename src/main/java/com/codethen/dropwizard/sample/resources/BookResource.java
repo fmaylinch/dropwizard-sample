db259c03c622446d7b3f94e88b632808266c32c5
fmaylinch <ferranmaylinch@gmail.com>
2017-08-10 12:17:13 +0200

    Use Mustache to generate the templates

--- src/main/java/com/codethen/dropwizard/sample/resources/BookResource.java
+++ src/main/java/com/codethen/dropwizard/sample/resources/BookResource.java
@@ -1,12 +1,17 @@
 package com.codethen.dropwizard.sample.resources;
 
 import com.codethen.dropwizard.sample.model.Book;
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,29 +31,29 @@ public class BookResource {
 	}
 
 	@GET
-	public String viewBooks() {
+	public String viewBooks() throws IOException {
 
-		String html = "<h1>Recommended books</h1>";
-		html += "<ul>";
-		for (Book book : books.values()) {
-			html += "<li><a href='/books/" + book.getId() + "'>" + book.getTitle() + "</a></li>";
-		}
-		html += "</ul>";
-
-		return html;
+		MustacheFactory mf = new DefaultMustacheFactory();
+		Mustache mustache = mf.compile("books.html");
+		StringWriter writer = new StringWriter();
+		mustache.execute(writer, books.values()).flush();
+		return writer.toString();
 	}
 
 	@GET
 	@Path("{id}")
-	public String viewBook(@PathParam("id") int id) {
+	public String viewBook(@PathParam("id") int id) throws IOException {
 
 		Book book = books.get(id);
 
 		if (book != null) {
-			String html = "<h1>" + book.getTitle() + "</h1>";
-			html += "<p>Author: " + book.getAuthor() + "</p>";
-			html += "<p>Pages: " + book.getNumPages() + "</p>";
-			return html;
+
+			MustacheFactory mf = new DefaultMustacheFactory();
+			Mustache mustache = mf.compile("book.html");
+			StringWriter writer = new StringWriter();
+			mustache.execute(writer, book).flush();
+			return writer.toString();
+
 		} else {
 			return "Book with id " + id + " not found!";
 		}
