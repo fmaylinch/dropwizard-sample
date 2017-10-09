8608d8476509f4e8b927028c30bf3898007caa71
fmaylinch <ferranmaylinch@gmail.com>
2017-10-09 20:42:44 +0200

    Get books from MySQL database using JDBC

---
+++ src/main/java/com/codethen/dropwizard/sample/JdbcExample.java
@@ -0,0 +1,36 @@
+package com.codethen.dropwizard.sample;
+
+import java.sql.*;
+
+public class JdbcExample {
+
+	public static void main(String[] args) throws SQLException {
+
+		String host = "localhost";
+		String schema = "books";
+		String user = "root";
+		String pwd = "maysicuel";
+
+		Connection conn = DriverManager.getConnection(
+			"jdbc:mysql://" + host + "/" + schema + "?user=" + user + "&password=" + pwd);
+
+
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("select * from books");
+
+		while (rs.next()) {
+
+			int id = rs.getInt("id");
+			String title = rs.getString("title");
+			String author = rs.getString("author");
+			int numPages = rs.getInt("num_pages");
+			Date releaseDate = rs.getDate("release_date");
+
+			System.out.println(id + ", " + title + ", " + author);
+		}
+
+		rs.close();
+		stmt.close();
+		conn.close();
+	}
+}
--- src/main/java/com/codethen/dropwizard/sample/service/BookService.java
+++ src/main/java/com/codethen/dropwizard/sample/service/BookService.java
@@ -2,44 +2,80 @@ package com.codethen.dropwizard.sample.service;
 
 import com.codethen.dropwizard.sample.model.Book;
 
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.*;
+import java.sql.Date;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class BookService {
 
-	private Map<Integer, Book> books;
-
-	private int nextId;
-
 	public BookService() {
 
-		books = new HashMap<>();
-		books.put(1,  new Book(1,  "Head first Java", "Kathy Sierra, Bert Bates", 720, new Date()) );
-		books.put(3,  new Book(3,  "Refactoring", "Martin Fowler", 464, new Date()) );
-		books.put(4,  new Book(4,  "Head first design patterns", "Eric Freeman, Beth Robson", 694, new Date()) );
-		books.put(5,  new Book(5, "Clean code", "Robert C. Martin", 288, new Date()) );
-
-		nextId = 6; // This would be managed by the database
 	}
 
 	public Book getById(int id) {
 
-		return books.get(id);
+		return null; // TODO books.get(id);
 	}
 
 	public Collection<Book> findByTitle(String search) {
 
 		final Collection<Book> result;
 
+		List<Book> books = getBooksFromDB();
+
 		if (search != null) {
-			result = this.books.values().stream()
+			result = books.stream()
 				.filter(book -> book.getTitle().toLowerCase().contains(search.toLowerCase()))
 				.collect(Collectors.toList());
 		} else {
-			result = this.books.values();
+			result = books;
+		}
+
+		return result;
+	}
+
+	private List<Book> getBooksFromDB() {
+
+		List<Book> result = new ArrayList<>();
+
+		String host = "localhost";
+		String schema = "books";
+		String user = "root";
+		String pwd = "maysicuel";
+
+
+		try {
+
+			// code that may fail
+			Connection conn = DriverManager.getConnection(
+				"jdbc:mysql://" + host + "/" + schema + "?user=" + user + "&password=" + pwd);
+
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from books");
+
+			while (rs.next()) {
+
+				// Extract data from current row
+				int id = rs.getInt("id");
+				String title = rs.getString("title");
+				String author = rs.getString("author");
+				int numPages = rs.getInt("num_pages");
+				Date releaseDate = rs.getDate("release_date");
+
+				// Add book to list
+				Book book = new Book(id, title, author, numPages, releaseDate);
+				result.add(book);
+			}
+
+			rs.close();
+			stmt.close();
+			conn.close();
+
+		} catch(SQLException e) {
+
+			// what to do in case of error???
+			throw new RuntimeException("there was a problem with the database", e);
 		}
 
 		return result;
@@ -47,12 +83,11 @@ public class BookService {
 
 	public Book addBook(Book book) {
 
-		book.setId(nextId);
+		// TODO
 
-		books.put(nextId, book);
-
-		nextId++;
-
+		//book.setId(nextId);
+		// books.put(nextId, book);
+		//nextId++;
 		return book;
 	}
 }
